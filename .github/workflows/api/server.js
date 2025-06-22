49cebdd656baad333587718e488fe40f7ff22cda
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-06-22 23:00:43 +0530

    Update and rename .github/workflows/server.js to .github/workflows/api/server.js

--- .github/workflows/api/server.js
+++ .github/workflows/api/server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res) => {
+  res.send('WebSocket server running');
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
-
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
